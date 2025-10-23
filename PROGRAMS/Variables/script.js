e9ebc5f5b224d10ed7d041c77ba2fbd600e48719
Abirlal Bera <abirlalbera24@gmail.com>
2025-10-24 00:36:20 +0530

    vault backup: 2025-10-24 00:36:20

--- PROGRAMS/Variables/script.js
+++ PROGRAMS/Variables/script.js
@@ -1,13 +1,17 @@
-//document.write("Hello, World!");
-//console.log("Hiii");
-
 var a = 10;
+let b = 20;
+const c = 30;
+d = 40; // Implicit global variable
 
 function f1()
 {
-    let b = 20;
-    console.log(a);
-    console.log(b);
+    var x = 100;// Local variable
+    y = 200;
+    console.log(x, y);
 }
 
 f1();
+
+console.log(x,y);// Error: x is not defined but y is accessible because it is implicitly global variable.
+
+console.log(y);
