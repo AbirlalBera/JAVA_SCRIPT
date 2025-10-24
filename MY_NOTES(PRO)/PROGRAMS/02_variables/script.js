1873d2e160fa05ed48f57d1a76efb09a7d1a719c
Abirlal Bera <abirlalbera24@gmail.com>
2025-10-24 19:17:37 +0530

    vault backup: 2025-10-24 19:17:37

---
+++ MY_NOTES(PRO)/PROGRAMS/02_variables/script.js
@@ -0,0 +1,8 @@
+let username = "Abirlal";
+let lastName = "Bera";
+let age = 25;
+let a = undefined;
+let isStudent = true;
+let Intro = `Hello, my name is`+ ` ` + username + ` ` + lastName + ` `+`My age is ` + age + `.`;
+
+console.log(Intro);
