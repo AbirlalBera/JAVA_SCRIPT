43496ce8a3acc5cd96b88c95d6d1e4be9ec9cf3c
Abirlal Bera <abirlalbera24@gmail.com>
2025-10-24 19:26:09 +0530

    vault backup: 2025-10-24 19:26:09

--- MY_NOTES(PRO)/PROGRAMS/02_variables/script.js
+++ MY_NOTES(PRO)/PROGRAMS/02_variables/script.js
@@ -1,4 +1,4 @@
-let username = "Abirlal";//
+let username = "Abirlal";//let : It is a block scoped variable. It means it can be accessed within the block it is defined.
 let lastName = "Bera";
 let age = 25;
 let a = undefined;
@@ -7,3 +7,15 @@ let Intro = `Hello, my name is`+ ` ` + username + ` ` + lastName + ` `+`My age i
 
 console.log(Intro);
 
+var city = "Kolkata";//var : It is a function scoped variable. It means it can be accessed within the function it is defined.
+var country = "India";
+var pincode = 700091;
+var isEmployed = false;
+var address = `I live in ` + city + `, ` + country + ` - ` + pincode + `.`;
+console.log(address);
+
+const pi = 3.14;//const : It is a block scoped variable. It means it can be accessed within the block it is defined. Its value cannot be changed.
+const gravity = 9.8;
+const speedOfLight = 299792458;
+const physicsConstants = `Some important physics constants are: Pi = ` + pi + `, Gravity = ` + gravity + ` m/s², Speed of Light = ` + speedOfLight + ` m/s.`;
+console.log(physicsConstants);  
